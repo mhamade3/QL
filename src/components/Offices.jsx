c3bba23c5ff870b360499cada5d736bd9f1cde20
mhamade3 <moehamade55@gmail.com>
2025-04-06 18:45:19 -0400

    hi

--- src/components/Offices.jsx
+++ src/components/Offices.jsx
@@ -21,17 +21,10 @@ export function Offices({ invert = false, ...props }) {
   return (
     <ul role="list" {...props}>
       <li>
-        <Office name="Copenhagen" invert={invert}>
-          1 Carlsberg Gate
+        <Office name="Detroit" invert={invert}>
+          123 Main St
           <br />
-          1260, København, Denmark
-        </Office>
-      </li>
-      <li>
-        <Office name="Billund" invert={invert}>
-          24 Lego Allé
-          <br />
-          7190, Billund, Denmark
+          Detroit, Michigan
         </Office>
       </li>
     </ul>
